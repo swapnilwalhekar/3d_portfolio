b7f3c23eb0c1ffd3f0c4fcca622b8a04de14baea
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2023-10-30 22:25:18 +0530

    GitIgnore file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Stars from "./components/canvas/Stars";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Feedbacks from "./components/Feedbacks";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+
 function App() {
   return (
     <>
-      <h1>3D Dev port</h1>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div>
+        <Contact/>
+        <Stars/>
+      </div>
+    </div>
+    </BrowserRouter>
     </>
   );
 }
