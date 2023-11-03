79d4e05e0bb6f509ebc3c7dcecabd6ff40706e8f
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2023-11-04 04:59:49 +0530

    Navbar for mobile responsiveness complted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState("");
+  const [active, setActive] = useState("");  // active title when click on the navbars
+  const [toggle, setToggle] = useState(false);
 
   return (
     <>
@@ -43,7 +44,36 @@ const Navbar = () => {
                 )
             })}
           </ul>
-          
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={toggle ? close : menu } alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=>setToggle(!toggle)}/>
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-5 my-2 max-w-[140px] z-10 rounded-xl`}>
+              <ul className="list-none flex flex-col justify-end items-start gap-3">
+                {navLinks.map((link)=>{
+                    return(
+                    <li
+                      key={link.id}
+                      className ={`${
+                        active === link.title
+                          ? "text-white"
+                          : "text-secondary"
+                        } font-poppins font-medium cursor-pointer text-[16px]`
+                      }
+                      onClick={()=>
+                        {
+                          setToggle(!toggle);
+                          setActive(link.title);
+                        }
+                      }
+                    >
+                      <a href = {`#${link.id}`}>{link.title}</a>
+                    </li>
+                    )
+                })}
+              </ul>
+            </div>
+          </div>
+
         </div>
       </nav>
     </>
