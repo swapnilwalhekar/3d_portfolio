e912b8930fafdfc23a1aa1f52386f6bde76dc5d4
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2023-12-06 23:40:57 +0530

    Computer glb loaded successfulyy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import tilt from 'react-tilt';
+import {motion} from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+// import {fadeIn, textVarient} from '../utils';
 
 const About = () => {
+  console.log("swapnil: fadeIn");
+
   return (
-    <div>About</div>
+    <>
+      <motion.div>
+        <p className={styles.heroSubText}> Introduction </p>
+        <h2 className={styles.heroHeadText}>Overview</h2>
+      </motion.div>
+    </>
   )
 }
 
