72bcbcf1b62ae3acefc8b0581944007f72caa93c
swapnilwalhekar <swapnilwalhekar1999@ggmail.com>
2023-12-14 23:36:45 +0530

    updating

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,12 +16,12 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
-  // github upload
+  
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{ max: 45, scale: 1, speed: 450 }}
-        className="bg-tertiary p-5 rouded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rouded-2xl sm:w-[330px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
@@ -42,6 +42,22 @@ const ProjectCard = ({
             </div>
           </div>
         </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {
+            tags.map((tag,index)=>{
+              return(
+                <p key={index} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
+              )
+            })
+          }
+        </div>
+
       </Tilt>
     </motion.div>
   );
