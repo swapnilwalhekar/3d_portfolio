046db87164bef228132b7026a257027beb043fb5
Swapnil Walhekar <108648658+swapnilwalhekar@users.noreply.github.com>
2023-12-15 17:45:02 +0530

    Add files via upload
    Completed functionality need to change all content and integrate emailjs account

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React from "react";
+import { useState, useRef } from "react";
+import { easeOut, motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import SectionWrapper from "../hoc/sectionWrapper";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    messege: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const formRef = useRef();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    emailjs
+      .send(
+        "seviceId",
+        "templateId",
+        {
+          from_name: form.name,
+          to_name: "Swapnil",
+          from_email: form.email,
+          to_email: "walhekar.swapnil2018@gmail.com",
+          message: form.messege,
+        },
+        "publicKey"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert(
+            `Thank You ${form.name}! I will get back to you as soon as possible.`
+          );
+
+          setForm({
+            name: "",
+            email: "",
+            messege: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong 😖");
+        }
+      );
+  };
+
+  return (
+    <div className="xl:mt12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rouded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get The</p>
+        <h3 className={styles.sectionHeadText}>Here</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows="7"
+              name="messege"
+              value={form.messege}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
+const Stars = (props) => {
+  const starRef = useRef();
+
+  useFrame((state, delta) => {
+    starRef.current.rotation.x -= delta / 10;
+    starRef.current.rotation.y -= delta / 15;
+  });
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
   return (
-    <div>Stars</div>
-  )
-}
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={starRef}
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
